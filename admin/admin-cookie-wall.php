e716b7e8e5fa1a57c7dcdceb7982915c1f0ef3c6
Robin Aldenhoven <robin@level-level.com>
2015-07-10 10:14:27 +0200

    automated htaccess

--- admin/admin-cookie-wall.php
+++ admin/admin-cookie-wall.php
@@ -9,7 +9,10 @@ class Admin_Cookie_Wall {
 				}
 			}
 		}
+
 		add_action( 'admin_menu', array( $this, 'register_cookie_wall_settings_submenu_page' ) );
+
+		add_action( 'admin_init', array($this, 'change_htaccess'));
 	}
 
 	public function register_cookie_wall_settings_submenu_page() {
@@ -38,7 +41,89 @@ class Admin_Cookie_Wall {
 		if( isset( $_POST['llcw_url'] ) ) {
 			$settings['page_url'] = $_POST['llcw_url'];
 		}
+
 		update_option( 'llcw_settings', $settings );
+
+		$this->change_htaccess();
+	}
+
+	private function create_htaccess(){
+		$blocked_agents = [
+			'Internet\ Explorer',
+			'MSIE',
+			'Chrome',
+			'Safari',
+			'Firefox',
+			'Windows',
+			'Opera',
+			'iphone',
+			'ipad',
+			'andriod',
+			'blackberry',
+		];
+		$agents = implode('|', $blocked_agents);
+
+		$new_htaccess = "# BEGIN Cookie Rewrite\n";
+		$new_htaccess .= "<IfModule mod_rewrite.c>\n";
+		
+		$new_htaccess .= "RewriteEngine On\n";
+
+		// Homepage
+		$host = $_SERVER['HTTP_HOST'];
+
+		$new_htaccess .= "RewriteCond %{HTTP_HOST} {$host} [NC]\n";
+		$new_htaccess .= "RewriteCond %{REQUEST_URI} ^/$\n";
+		$new_htaccess .= "RewriteCond %{HTTP_COOKIE} !^.*ll_cookie_wall.*\n";
+		$new_htaccess .= "RewriteCond %{HTTP_USER_AGENT} {$agents} \n";
+		$new_htaccess .= "RewriteRule .* /cookie-wall?url_redirect=http%1://%{HTTP_HOST}%{REQUEST_URI} [R=302,L]\n\n";
+
+
+		// All other pages
+		$new_htaccess .= "RewriteCond %{REQUEST_URI} !^/cookie-wall.*\n";
+		$new_htaccess .= "RewriteCond %{HTTP_COOKIE} !^.*ll_cookie_wall.*\n";
+		$new_htaccess .= "RewriteCond %{REQUEST_URI} !index.php\n";
+		$new_htaccess .= "RewriteCond %{REQUEST_FILENAME} !-f\n";
+		$new_htaccess .= "RewriteCond %{REQUEST_FILENAME} !-d\n";
+		$new_htaccess .= "RewriteCond %{HTTP_USER_AGENT} {$agents} \n";
+		$new_htaccess .= "RewriteRule .* /cookie-wall?url_redirect=http%1://%{HTTP_HOST}%{REQUEST_URI} [R=302,L] \n";
+
+		$new_htaccess .= "</IfModule>\n";
+		$new_htaccess .= "# END Cookie Rewrite\n\n\n";
+
+		return $new_htaccess;
+	}
+
+	public function change_htaccess(){
+		global $wp_filesystem;
+
+		// Get filesystem creds
+		$url = wp_nonce_url(admin_url('options-general.php?page=ll-cookie-wall-settings'));
+		if (false === ($creds = request_filesystem_credentials($url, '', false, false, null) ) ) 
+			return; // stop processing here
+
+		// Check filesystem creds
+		if(!WP_Filesystem($creds)) 
+			return false;
+
+		// Check if .htaccess exists
+		$root = get_home_path();
+		$htaccess_path = $root . '.htaccess';
+
+		if( !$wp_filesystem->exists($root . '.htaccess') )
+			return;
+
+		$orginal_htaccess = $wp_filesystem->get_contents($htaccess_path);
+		
+		// Remove Cookie rewrites
+		$orginal_htaccess = preg_replace('/(\# BEGIN Cookie Rewrite.*\# END Cookie Rewrite)/s', '', $orginal_htaccess);
+		$orginal_htaccess = trim($orginal_htaccess);
+
+
+		// Add Rewrite
+		$new_htaccess = $this->create_htaccess();
+		$new_htaccess .= $orginal_htaccess;
+
+		// Update Cookie rewrites
+		$wp_filesystem->put_contents($htaccess_path, $new_htaccess);
 	}
 }
-
